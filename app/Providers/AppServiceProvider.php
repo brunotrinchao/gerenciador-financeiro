c6fa55e401c2dd4dd0528f2225aaabf40a4e2e63
brunotrinchao <bruno.souza@onfly.com.br>
2025-05-20 15:17:30 -0300

    Corrige URLs para HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,5 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if(env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
