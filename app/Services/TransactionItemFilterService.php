3fee6df6b4263cfc21f21c8e7e2eebf358b211b5
brunotrinchao <bruno.souza@onfly.com.br>
2025-07-18 19:17:49 -0300

    Add selection multi language

--- app/Services/TransactionItemFilterService.php
+++ app/Services/TransactionItemFilterService.php
@@ -55,55 +55,8 @@ class TransactionItemFilterService
     {
         return $this->items()
             ->with(['transaction', 'card', 'account.bank'])
-            ->where('status', '=', 'PENDING')
+            ->where('status', '<>', 'PAID')
             ->orderBy('due_date')
             ->limit(10);
-    }
-
-    public function updateAmountAndInstallmentCount(TransactionItem $transactionItem): void
-    {
-        $transaction = $transactionItem->transaction();
-
-        $paidItems = TransactionItem::where('transaction_id', $transaction->id)
-            ->where('status', '=', 'PAID')
-            ->get();
-        $remainingItems = TransactionItem::where('transaction_id', $transaction->id)
-            ->where('status', '!=', 'PAID')
-            ->get();
-
-        $totalPaid = $paidItems->sum('amount');
-
-        $amount = $transaction->amount - $totalPaid;
-        $installmentsCount = $remainingItems->count();
-
-        $baseValue = floor($amount / $installmentsCount * 100) / 100; // força 2 casas
-        $remaining = $amount - ($baseValue * $installmentsCount);
-
-
-        $installmentCount = $paidItems->count();
-        $lastPaidInstallmentNumber = $paidItems->max('installment_number') ?? 0;
-        $startDate = Carbon::parse($transaction->date);
-
-        $cardDueDay = null;
-        if ($transaction->method === 'CARD' && $transaction->card_id) {
-            $cardDueDay = $transaction->card->due_date; // Ex: 23
-        }
-
-        foreach ($remainingItems as $i => $item) {
-            $installmentNumber = $lastPaidInstallmentNumber + $i + 1;
-            $currentAmount = ($installmentCount === $installmentsCount) ? $baseValue + $remaining : $baseValue;
-
-            $dueDate = (clone $startDate)->addMonths($installmentNumber);
-
-            if ($cardDueDay) {
-                $dueDate->day = min($cardDueDay, $dueDate->daysInMonth);
-            }
-
-            $item->update([
-                'amount' => $currentAmount,
-                'installment_number' => $installmentNumber,
-                'due_date' => $dueDate,
-            ]);
-        }
     }
 }
