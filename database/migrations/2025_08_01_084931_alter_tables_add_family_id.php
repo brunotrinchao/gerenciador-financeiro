2621d6dba8df1096feddcd20b1df76287e7a5a64
brunotrinchao <bruno.souza@onfly.com.br>
2025-08-05 16:22:49 -0300

    WIP: alterações no migration add_family_id

--- database/migrations/2025_08_01_084931_alter_tables_add_family_id.php
+++ database/migrations/2025_08_01_084931_alter_tables_add_family_id.php
@@ -48,7 +48,6 @@ return new class extends Migration
             'accounts',
             'cards',
             'transactions',
-            'job',
             'category',
             'notifications',
             'action_logs',
